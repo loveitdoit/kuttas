c3e53c46395974faa1a5ffdb576ebc9be7ce5f78
loveitdoit <183669953@qq.com>
2016-07-11 18:39:22 +0800

    express 测试

--- app.js
+++ app.js
@@ -1,42 +1,50 @@
 var express = require('express');
 var app = express();
-var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express3-handlebars').create({
+    defaultLayout: 'main'
+});
 
-app.engine('handlebars',handlebars.engine);
-app.set('view engine','handlebars');
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
-app.set('port',process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',function (req,res) {
-    // res.type('text/plain');
-    // res.send('Kuttas Home');
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
+app.get('/', function(req, res) {
     res.render('home');
 });
 
-app.get('/about',function (req,res) {
-    // res.type('text/plain');
-    // res.send('About Kuttas');
-    res.render('about');
+app.get('/about', function(req, res) {
+    res.render('about', {
+        pageTestScript: '/qa/tests-about.js'
+    });
 });
 
-app.use(function (req,res) {
-    // res.type('text/plain');
+app.get('/tours/hood-river', function(req, res) {
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res) {
+    res.render('tours/request-group-rate');
+});
+
+app.use(function(req, res) {
     res.status(404);
     res.render('404');
-    // res.send('404 - Not Found');
 });
 
-app.use(function (err,req,res,next) {
+app.use(function(err, req, res, next) {
     console.log(err.stack);
-    // res.type('text/plain');
     res.status(500);
     res.render('500');
-    // res.send('500 - Server Error');
 });
 
-app.listen(app.get('port'),function () {
-    console.log('Express tarted on http://localhost:'+
-    app.get('port')+';press Ctrl - C to teminate.');
+app.listen(app.get('port'), function() {
+    console.log('Express tarted on http://localhost:' +
+        app.get('port') + ';press Ctrl - C to teminate.');
 });
